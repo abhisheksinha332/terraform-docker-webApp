8b93d014210e494d6ac899572e9fc1a316aaaf76
Abhishek Sinha <3327abhishek@gmail.com>
2025-07-20 18:34:11 +0530

    Updated variables

--- main.tf
+++ main.tf
@@ -20,13 +20,13 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "react-app-rg" {
-  name     = "react-app-rg"
+  name     = var.resource_group
   location = "West Europe"
   
 }
 
 resource "azurerm_app_service_plan" "react-app-service-plan" {
-  name                = "react-app-service-plan"
+  name                = var.app-service_plan_name
   location            = azurerm_resource_group.react-app-rg.location
   resource_group_name = azurerm_resource_group.react-app-rg.name
   kind = "Linux"
@@ -38,14 +38,14 @@ resource "azurerm_app_service_plan" "react-app-service-plan" {
 }
 
 resource "azurerm_linux_web_app" "react-app" {
-  name                = "react-app"
+  name                = var.web_app_name
   location            = azurerm_resource_group.react-app-rg.location
   resource_group_name = azurerm_resource_group.react-app-rg.name
   service_plan_id     = azurerm_app_service_plan.react-app-service-plan.id
 
  site_config {
     application_stack {
-      docker_image_name     = "abhisheksinha332/test-aks:0.0.4.RELEASE"
+      docker_image_name     = var.docker_image_name
     }
     always_on = true
   }
--- variables.tf
+++ variables.tf
@@ -20,3 +20,22 @@ variable "subscription_id" {
 }
 
 
+variable "resource_group" {
+  description = "Resource group for the Azure resources"
+  type        = string
+}
+
+variable "app-service_plan_name" {
+  description = "Name of the App Service Plan"
+  type        = string
+  
+}
+variable "web_app_name" {
+  description = "Name of the Web App"
+  type        = string
+}
+
+variable "docker_image_name" {
+  description = "Docker image name for the web app"
+  type        = string
+}
